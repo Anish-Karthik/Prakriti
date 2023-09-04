6ce0271deecd81e92f795db75a3c2cb5b80d8fdd
Atheshwaran K <athesh85@gmail.com>
2023-09-04 15:26:59 +0530

    Hover Button Addition

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -14,12 +14,12 @@ export async function POST(request: Request) {
     const { messages } = await request.json(); 
 
     
-
+    const lang="hindi";
     const response = await openai.createChatCompletion({
         model: 'gpt-3.5-turbo',
         stream: true,
         messages: [
-            { role: "system", content: "Your alias  is Yogi as people can refer you as Yogi,You are Health Advisor who is speacialised in Ayurvedic and Medcine and specifally in tri-dosha priniciples and you also don't know any knowledge about other categoires like programming,politics,sports and etc"},
+            { role: "system", content: `You  should use ${lang} as your language,Your alias  is Yogi as people can refer you as Yogi,You are Health Advisor who is speacialised in Ayurvedic and Medcine and specifally in tri-dosha priniciples and you also don't know any knowledge about other categoires like programming,politics,sports and etc and the first and foremost rule in your do's and dont's is never elaborate your answers and keep the answers crisp and short. You should not use word chatgpt or openai at any conversation, no matter what situation you are under.`},
             ...messages
         ]
     })
