307633e3d02d2d807051dac0928c10f9deaeb863
Atheshwaran K <athesh85@gmail.com>
2023-09-17 12:31:31 +0530

    FAQ PAGE UI

--- app/(dashboard)/(routes)/faq/page.tsx
+++ app/(dashboard)/(routes)/faq/page.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+'use client';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
 
-const page = () => {
+export function FAQ() {
   return (
-    <div>Page Under Devleopment</div>
+    <div className="mt-4 pt-2 px-8 bg-gray-50 justify-center rounded-md ">
+      <h2 className="font-bold text-center text-2xl">
+        FAQ
+      </h2>
+  <Accordion type="single" collapsible className="w-full">
+      <AccordionItem value="item-1">
+        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It adheres to the WAI-ARIA design pattern.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-2">
+        <AccordionTrigger>Is it styled?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It comes with default styles that matches the other
+          components&apos; aesthetic.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-3">
+        <AccordionTrigger>Is it animated?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It's animated by default, but you can disable it if you prefer.
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>  
+    </div>
+    
   )
 }
 
-export default page
+export default FAQ;
