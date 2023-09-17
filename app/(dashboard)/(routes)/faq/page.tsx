54761d1ae0656c6213a28d38dd75fed1e9f1d67a
Atheshwaran K <athesh85@gmail.com>
2023-09-17 12:42:28 +0530

    FAQ PAGE UI

--- app/(dashboard)/(routes)/faq/page.tsx
+++ app/(dashboard)/(routes)/faq/page.tsx
@@ -14,27 +14,6 @@ export const faq = () => {
       <h2 className="font-bold text-center text-2xl">
         FAQ
       </h2>
-  <Accordion type="single" collapsible className="w-full">
-      <AccordionItem value="item-1">
-        <AccordionTrigger>Is it accessible?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It adheres to the WAI-ARIA design pattern.
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="item-2">
-        <AccordionTrigger>Is it styled?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It comes with default styles that matches the other
-          components&apos; aesthetic.
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="item-3">
-        <AccordionTrigger>Is it animated?</AccordionTrigger>
-        <AccordionContent>
-          Yes. Its animated by default, but you can disable it if you prefer.
-        </AccordionContent>
-      </AccordionItem>
-    </Accordion>  
     </div>
     
   )
