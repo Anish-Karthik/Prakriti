7c8f9e0813e34d5fd196bcf35409823d1a1cff88
Atheshwaran K <athesh85@gmail.com>
2023-09-17 12:54:24 +0530

    FAQ PAGE UI

--- app/(dashboard)/(routes)/faq/page.tsx
+++ app/(dashboard)/(routes)/faq/page.tsx
@@ -9,20 +9,19 @@ const page = () => {
       <AccordionItem value="item-1">
         <AccordionTrigger>Is it accessible?</AccordionTrigger>
         <AccordionContent>
-          Yes. It adheres to the WAI-ARIA design pattern.
+          Yes
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-2">
         <AccordionTrigger>Is it styled?</AccordionTrigger>
         <AccordionContent>
-          Yes. It comes with default styles that matches the other
-          components&apos; aesthetic.
+          Yes
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-3">
         <AccordionTrigger>Is it animated?</AccordionTrigger>
         <AccordionContent>
-          Yes. It's animated by default, but you can disable it if you prefer.
+          Yes
         </AccordionContent>
       </AccordionItem>
     </Accordion>
