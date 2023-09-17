754594fbdc647d7df146d98a0b2b9007cef190ad
Atheshwaran K <athesh85@gmail.com>
2023-09-17 12:53:05 +0530

    FAQ PAGE UI

--- app/(dashboard)/(routes)/faq/page.tsx
+++ app/(dashboard)/(routes)/faq/page.tsx
@@ -1,8 +1,32 @@
+'use client';
+import { AccordionTrigger,Accordion,AccordionItem,AccordionContent } from '@/components/ui/accordion';
 import React from 'react'
 
 const page = () => {
   return (
-    <div>Page Under Devleopment</div>
+    <div>
+      <Accordion type="single" collapsible className="w-full">
+      <AccordionItem value="item-1">
+        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It adheres to the WAI-ARIA design pattern.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-2">
+        <AccordionTrigger>Is it styled?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It comes with default styles that matches the other
+          components&apos; aesthetic.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-3">
+        <AccordionTrigger>Is it animated?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It's animated by default, but you can disable it if you prefer.
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+    </div>
   )
 }
 
