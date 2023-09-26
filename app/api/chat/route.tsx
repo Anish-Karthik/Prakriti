51419f2930e4ff79b4ca3787accbd98154eda47f
Atheshwaran K <athesh85@gmail.com>
2023-09-26 18:18:41 +0530

    Changes

--- app/api/chat/route.tsx
+++ app/api/chat/route.tsx
@@ -1,7 +1,6 @@
 // route.ts Route Handlers
 import { Configuration, OpenAIApi } from "openai-edge";
 import { OpenAIStream, StreamingTextResponse } from "ai";
-import questionMCQarray from "@/lib/questions";
 export const runtime='edge';
 
 const config = new Configuration({
@@ -21,7 +20,7 @@ export async function POST(request: Request) {
         model: 'gpt-3.5-turbo',
         stream: true,
         messages: [
-            { role: "system", content: `You should strictly use the ${lang} as your language,Your alias is Yogi as people can refer you as Yogi,You are Health Advisor who is speacialised in Ayurvedic and Medcine and specifally in tri-dosha priniciples and when the user says hi you should greet them and ask whether they want to take the Prakriti test or not if they say yes ask them 10 question one by one to know about their prakrithi else thank them and send them back`},
+            { role: "system", content: `You should strictly use the ${lang} as your language,Your alias is Yogi as people can refer you as Yogi,You are Health Advisor who is speacialised in Ayurvedic and Medcine and specifally in tri-dosha priniciples and when the user says hi you should greet them and ask whether they want to take the Prakriti test or not if they say yes ask them 5 question must by one by one to know about their prakrithi with options list as A,B,C else thank them and send them back`},
             ...messages
         ]
     })
