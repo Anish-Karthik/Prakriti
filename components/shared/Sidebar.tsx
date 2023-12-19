8fe7aa804fc1c21b57ea4f51e259bbcb77ae52fd
Atheshwaran K <athesh85@gmail.com>
2023-12-19 21:19:45 +0530

    Nextauth

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -3,7 +3,8 @@
 import { Montserrat } from "next/font/google"
 import Image from "next/image"
 import Link from "next/link"
-import { usePathname } from "next/navigation"
+import { usePathname, useRouter } from "next/navigation"
+import { HiArrowLeftOnRectangle, HiUsers } from 'react-icons/hi2';
 import { NEXT_PUBLIC_APP_NAME } from "@/public/constants"
 import {
   Apple,
@@ -27,6 +28,8 @@ const montserrat = Montserrat({
 
 
   import { useEffect } from "react"
+import { Button } from "../ui/button"
+import { signOut } from "next-auth/react"
 
 
 // get current path
@@ -53,6 +56,7 @@ const Sidebar = () => {
     window.googleTranslateElementInit = googleTranslateElementInit;
   }, []);
   const pathname = usePathname()
+  const router=useRouter()
   return (
     <div className="space-y-4 py-2 flex flex-col h-full bg-[#111827] text-white ">
       <div className="px-3 py-2 flex-1">
@@ -67,28 +71,36 @@ const Sidebar = () => {
 
         <div className="space-y-1.5 pt-3 lg:pt-4">
           {routes.map((route) => (
-            <Link
-              key={route.label}
-              href={route.href}
-              className={cn(
-                "text-sm group flex p-3 w-full justify-start cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition ",
-                montserrat.className,
-                pathname === route.href
-                  ? "bg-white/50 !important text-black/90"
-                  : ""
-              )}
-            >
-              <div className="flex items-center flex-1">
-                <route.icon
-                  className={cn("w-5 h-5 mr-2", route.color)}
-                  aria-hidden="true"
-                />
-                {route.label}
-              </div>
-            </Link>
-          ))}
+          <div
+            key={route.label}
+            onClick={async () => {
+              if (route.label === "Logout") {
+                await signOut();
+                router.push(route.href);
+              } else {
+                router.push(route.href);
+              }
+            }}
+            className={cn(
+              "text-sm group flex p-3 w-full justify-start cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition ",
+              montserrat.className,
+              pathname === route.href
+                ? "bg-white/50 !important text-black/90"
+                : ""
+            )}
+          >
+            <div className="flex items-center flex-1">
+              <route.icon
+                className={cn("w-5 h-5 mr-2", route.color)}
+                aria-hidden="true"
+              />
+              {route.label}
+            </div>
+          </div>
+        ))}
         </div>
          <div className="hidden" id="google_translate_element"></div>
+        
       </div>
     </div>
   )
@@ -121,6 +133,7 @@ export const routes: {
     href: "/diet-plan",
     color: "text-red-500",
   },
+ 
   // {
   //   label: "Ayur-Unity",
   //   icon: Users,
@@ -157,5 +170,11 @@ export const routes: {
     icon: SettingsIcon,
     href: "/settings",
     color: "text-white-500",
+  },
+   {
+      label: 'Logout', 
+      href: '/sign-in',
+      icon: HiArrowLeftOnRectangle, 
+      color: "text-white-500",
   },
 ]
